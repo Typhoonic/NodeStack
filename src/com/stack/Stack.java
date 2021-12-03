69b289559b3cec528789b96c4cc6d183750e1b62
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-12-03 11:16:22 +0100

    little fix

--- src/com/stack/Stack.java
+++ src/com/stack/Stack.java
@@ -5,53 +5,54 @@ public class Stack {
     private int length = 0;
     private Node top = null;
 
-    public void push(int item){
+    public void push(int item) {
         Node node = new Node(item);
-        if (top == null){
-            top = node;
-        } else {
+
+        if (top != null) {
             node.next = top;
-            top = node;
         }
 
-        length ++;
+        top = node;
+        length++;
     }
 
-    public int pop(){
+    public int pop() {
         int result = -1;
 
-        if(top!=null) {
+        if(top != null) {
             result = top.data;
             top = top.next;
-            length --;
+            length--;
         }
 
         return result;
     }
 
-    public int peekMin(){
+    public int peekMin() {
         int min = 0;
 
-        if(top!=null)
+        if(top != null)
             min = top.data;
 
         for (int i = 0; i < size() - 1; i++) {
             top = top.next;
+
             if (min > top.data)
                 min = top.data;
         }
+
         return min;
     }
 
-    public int size(){
+    public int size() {
         return length;
     }
 
-    private class Node{
+    private class Node {
         private Node next = null;
         private int data = 0;
 
-        private Node(int value){
+        private Node(int value) {
             this.data = value;
         }
     }
